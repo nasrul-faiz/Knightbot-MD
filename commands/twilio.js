9af8acf8d05d10eff49523644877d9b111ff9108
nasrul-faiz <nasrulfaizz1997@gmail.com>
2026-06-20 15:01:07 +0000

    initial commit

--- commands/twilio.js
+++ commands/twilio.js
@@ -3,22 +3,21 @@ const { sendInteractiveButtons } = require('../lib/interactiveButtons');
 
 async function twilioCommand(sock, chatId, message) {
     await sendInteractiveButtons(sock, chatId, {
-        text: `Hello! I am ${settings.botName}. Choose an option below to get started.`,
+        text: `Hello! I am ${settings.botName}. Choose an option below to get started.\n\nRepo: https://github.com/gatotkacabatu999-lab/Knightbot-MD\nOwner: +${settings.ownerNumber}`,
         footer: `${settings.botName} • by ${settings.botOwner}`,
         nativeButtons: [
             {
-                name: 'cta_url',
+                name: 'quick_reply',
                 buttonParamsJson: JSON.stringify({
-                    display_text: '🌐 Visit GitHub',
-                    url: 'https://github.com/gatotkacabatu999-lab/Knightbot-MD',
-                    merchant_url: 'https://github.com/gatotkacabatu999-lab/Knightbot-MD'
+                    display_text: '🌐 Open Repo',
+                    id: '.github'
                 })
             },
             {
-                name: 'cta_call',
+                name: 'quick_reply',
                 buttonParamsJson: JSON.stringify({
-                    display_text: '📞 Contact Owner',
-                    id: `+${settings.ownerNumber}`
+                    display_text: '📞 Owner',
+                    id: '.owner'
                 })
             },
             {
