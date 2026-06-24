aa7e9a03b2037309e5009b32e32c0696e911d9b2
nasrul-faiz <nasrulfaizz1997@gmail.com>
2026-06-24 04:13:26 +0000

    initial commit

--- node_modules/@whiskeysockets/baileys/lib/Types/Message.js
+++ node_modules/@whiskeysockets/baileys/lib/Types/Message.js
@@ -1,11 +1,7 @@
-import { proto } from '../../WAProto/index.js';
-// export the WAMessage Prototypes
-export { proto as WAProto };
-export const WAMessageStubType = proto.WebMessageInfo.StubType;
-export const WAMessageStatus = proto.WebMessageInfo.Status;
-export var WAMessageAddressingMode;
-(function (WAMessageAddressingMode) {
-    WAMessageAddressingMode["PN"] = "pn";
-    WAMessageAddressingMode["LID"] = "lid";
-})(WAMessageAddressingMode || (WAMessageAddressingMode = {}));
-//# sourceMappingURL=Message.js.map
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WAMessageStatus = exports.WAMessageStubType = exports.WAProto = void 0;
+const WAProto_1 = require("../../WAProto");
+Object.defineProperty(exports, "WAProto", { enumerable: true, get: function () { return WAProto_1.proto; } });
+exports.WAMessageStubType = WAProto_1.proto.WebMessageInfo.StubType;
+exports.WAMessageStatus = WAProto_1.proto.WebMessageInfo.Status;
